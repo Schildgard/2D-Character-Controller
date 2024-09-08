a3a8336058fef0398d17338953d2667ecc401696
Schildgard <schildgard@gmail.com>
2024-09-08 12:46:13 +0200

    Camera Change
    Camera is now draggable while pressing left Mouse button and is no longer moved with W or S.

--- 2D Controller/Assets/Scripts/Controller/CameraController.cs
+++ 2D Controller/Assets/Scripts/Controller/CameraController.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class CameraController : MonoBehaviour
 {
-    // Camera follows the Player and when pressed Up or Down, scrolls down a bit
-    private float verticalInput;
+    // Camera follows the Player and when and is dragable ony the Y axis
+
     private const float cameraDistance = -10;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private float lerpSpeed;
     [SerializeField] private float maxCameraDisplacement;
 
+    private Vector3 initialMousePosition;
+
+    private Vector3 camPos;
     private void Update()
     {
-        verticalInput = Input.GetAxis("Vertical");
-        transform.position = Vector3.Lerp(playerTransform.position, new Vector3(playerTransform.position.x, playerTransform.position.y + (verticalInput * maxCameraDisplacement), cameraDistance), lerpSpeed);
+        // Get Mouse Position on Button Press to be able to calculate the mount movement
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialMousePosition = Input.mousePosition;
+        }
+        // Calculate how far and in which direction the mouse has moved
+        if (Input.GetMouseButton(0))
+        {
+            float positionDifference = Mathf.Clamp(Input.mousePosition.y - initialMousePosition.y, -maxCameraDisplacement, maxCameraDisplacement);
+            camPos = new Vector3(playerTransform.position.x, playerTransform.position.y + positionDifference, cameraDistance);
+            transform.position = Vector3.Lerp(transform.position, camPos,lerpSpeed*Time.deltaTime);
+        }
+        else transform.position = Vector3.Lerp(transform.position, new Vector3(playerTransform.position.x, playerTransform.position.y,cameraDistance), lerpSpeed*Time.deltaTime);
     }
 }
